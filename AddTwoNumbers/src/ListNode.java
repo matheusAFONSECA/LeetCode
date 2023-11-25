2c3762ade8af2baea1351eb4bef13c0d51c667ac
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-11-25 11:44:56 -0300

    Exemplo de como usar o ListNode

---
+++ AddTwoNumbers/src/ListNode.java
@@ -0,0 +1,11 @@
+public class ListNode {
+    // var aux
+    int val;
+    ListNode next;
+
+    // List node -> "nó de list" - aux na criação de laços entre um valor e outro - Árvore de nós
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
--- AddTwoNumbers/src/Main.java
+++ AddTwoNumbers/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Criando uma lista encadeada com três nós
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+
+        // Estabelecendo as conexões entre os nós
+        node1.next = node2;
+        node2.next = node3;
+
+        // Percorrendo e imprimindo os valores da lista encadeada
+        ListNode current = node1;
+        while (current != null) {
+            System.out.println(current.val);
+            current = current.next;
+        }
+
     }
 }
