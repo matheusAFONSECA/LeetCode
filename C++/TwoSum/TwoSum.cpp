f8371dbe23df54f7d270e3994002728fe9010ff6
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-11-25 19:04:25 -0300

    Everything in directorys to separate problens

---
+++ C++/TwoSum/TwoSum.cpp
@@ -0,0 +1,15 @@
+// importação de bibliotecas
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // var aux
+    int alvo;           // valor que desejamos alcançar
+    int vetor[50];      // vetor que faremos a análise da soma
+    int posicao[50];    // vetor que guardamos o valor da posição do vetor que dá a soma
+
+    // entrada de dados
+    
+
+}
