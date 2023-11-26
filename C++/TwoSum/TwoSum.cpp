bc904e0c54c7b6613ee1ead13442a8776dc3e871
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-11-26 16:26:53 -0300

    Case 3 is giving a problem

--- C++/TwoSum/TwoSum.cpp
+++ C++/TwoSum/TwoSum.cpp
@@ -7,24 +7,49 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // var aux
-        vector<int> result;     // definindo um vetor aux para pegar as posições
+        vector<int> result;         // definindo um vetor aux para pegar as posições
         int sum = 0;                // guarda a soma
+        int verifica = target;      // ajuda a verificar se a resposta foi a certa
+        int i = 0;                  // var de controle do for
+        int j = 0;                  // var de controle do while
 
-        // passando pelo vetor
-        for(int i = 0; i < nums.size(); i++){
+        while(j != -1)
+        {
+            // passando pelo vetor
+            for(i; i < nums.size(); i++){
 
-            // realizando a soma do valor
-            sum = sum + nums[i];
-            // cout << "soma:" << sum << endl;
+                // realizando a soma do valor
+                sum = sum + nums[i];
+                // cout << "soma:" << sum << endl;
 
-            // definindo condição para pegar a posição do vetor
-            if (sum <= target)
-            {
-                result.push_back(i);    // pegando a posição do vetor
+                // definindo condição para pegar a posição do vetor
+                if (sum <= target)
+                {
+                    result.push_back(i);    // pegando a posição do vetor
+                    verifica = sum;
+                }
+                
             }
-            
-        }
 
+            // verifica se pode sair do while -> caso contrário faz o for novamente, mas com uma posição na frente
+            if (verifica != target)
+            {
+                j++;                // incrementa a posição que o loop for vai começar
+                i = j;              // define a nova posição que o loop for vai ocorrer
+                sum = 0;            // zera a soma
+                result.clear();     // limpa as posições pegas de forma errada
+
+                if (j == nums.size())   // caso não tenha como conseguir o valor alvo
+                {
+                    j = -1;             // sai do loop while
+                    result.clear();     // limpa as posições pegas de forma errada
+                }
+                
+            } else {
+                j = -1;             // sai do loop while
+            }
+        }
+        
         // retornando os vetores
         return result;
     }
@@ -38,13 +63,30 @@ int main() {
     Solution s;             // instanciando o objeto Solution para usar a função twoSum
 
     // Adicionando elementos ao vetor usando o método push_back() para adicionar valores no vetor
+
+    // case 1
+    // numeros.push_back(2);
+    // numeros.push_back(7);
+    // numeros.push_back(11);
+    // numeros.push_back(19);
+
+    // case 2
+    // numeros.push_back(3);
+    // numeros.push_back(2);
+    // numeros.push_back(4);
+    
+    // case 3
+    numeros.push_back(3);
     numeros.push_back(2);
-    numeros.push_back(7);
-    numeros.push_back(11);
-    numeros.push_back(15);
+    numeros.push_back(3);
 
     // definindo o alvo da soma
-    alvo = 9;
+    
+    //case 1
+    // alvo = 9;
+
+    // case 2 e 3
+    alvo = 6;
 
     // chamando a função solution e mandando o valor para um vector
     resultado = s.twoSum(numeros, alvo);
