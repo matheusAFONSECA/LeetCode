6bb02aefb045bb741bbbeddb62b31742ab5a32cb
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-11-26 17:05:11 -0300

    base code to solve the problem '3Sum'

---
+++ C++/3Sum/3Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+    }
+};
+
+int main(){
+
+
+    return 0;
+}
