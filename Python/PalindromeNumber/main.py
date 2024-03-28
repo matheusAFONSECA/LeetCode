f4237630ec05b5eeb575e4bc2956ba0ff4231517
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-28 16:16:35 -0300

    Base code and cases

---
+++ Python/PalindromeNumber/main.py
@@ -0,0 +1,21 @@
+from solution import Solution
+
+s = Solution()
+
+# case 1
+print("Case 1")
+print("Input: 121")
+print("Output: ", s.isPalindrome(121))
+print("Expected: True")
+
+# case 2
+print("Case 2")
+print("Input: -121")
+print("Output: ", s.isPalindrome(-121))
+print("Expected: False")
+
+# case 1
+print("Case 3")
+print("Input: 10")
+print("Output: ", s.isPalindrome(10))
+print("Expected: False")
