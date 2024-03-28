a3e888c093fef034a32e6dd6be7f8018d9c0a0bb
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-28 16:25:55 -0300

    Problem solved

--- Python/PalindromeNumber/solution.py
+++ Python/PalindromeNumber/solution.py
@@ -1,6 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
 
+        # um n° negativo não pode ser um palíndromo
+        if x < int(0):
+            return False
+        
+        else:
+            # Converte o valor para uma string, inverte a ordem dos caracteres e converte de volta para inteiro
+            x_invertido = int(str(x)[::-1])
 
-        return True
+            if x == x_invertido:
+                return True
+            else:
+                return False
     
