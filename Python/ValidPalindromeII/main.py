97dd687da5931029baf73c8bc7f8ecd2f1f393f0
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-30 01:27:18 -0300

    base code

---
+++ Python/ValidPalindromeII/main.py
@@ -0,0 +1,31 @@
+from solution3 import Solution
+
+s2 = Solution()
+
+# case 1
+print("Case 1")
+print("Input: s = 'A man, a plan, a canal: Panama'")
+s = 'A man, a plan, a canal: Panama'
+print("Output: ", s2.isPalindrome(s))
+print("Expected: True")
+
+# case 2
+print("Case 2")
+print("Input: s = 'race a car'")
+s = 'race a car'
+print("Output: ", s2.isPalindrome(s))
+print("Expected: False")
+
+# case 3
+print("Case 3")
+print("Input: s = ' '")
+s = ' '
+print("Output: ", s2.isPalindrome(s))
+print("Expected: True")
+
+# case 4
+print("Case 4")
+print("Input: s = '0P'")
+s = '0P'
+print("Output: ", s2.isPalindrome(s))
+print("Expected: False")
