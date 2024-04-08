ae933083923f52211f52b96e5217e93a662afc4b
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-04-08 17:31:05 -0300

    Problem solved

--- Python/NumberOfLongestIncreasingSubsequence.py
+++ Python/NumberOfLongestIncreasingSubsequence.py
@@ -1,21 +1,22 @@
 class Solution:
-    def findLengthOfLCIS(self, nums) -> int:
+    def findNumberOfLIS(self, nums) -> int:
+      
+        # Initialize two arrays to store the length of longest increasing subsequences and their corresponding counts
+        lengths = [1] * len(nums)
+        counts = [1] * len(nums)
 
-        max_length = 1  # Comprimento máximo da sequência crescente
-        current_length = 1  # Comprimento atual da sequência crescente
-        
-        # Percorre os elementos da lista, começando do segundo elemento
-        for i in range(1, len(nums)):
-            # Se o elemento atual for maior que o elemento anterior, a sequência continua
-            if nums[i] > nums[i - 1]:
-                current_length += 1
-                # Atualiza o comprimento máximo, se necessário
-                max_length = max(max_length, current_length)
-            else:
-                # Se não, reinicia o comprimento atual da sequência
-                current_length = 1
-        
-        return max_length
+        # Traverse the array to find the longest increasing subsequence length and their counts
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if lengths[j] + 1 > lengths[i]:
+                        lengths[i] = lengths[j] + 1
+                        counts[i] = counts[j]
+                    elif lengths[j] + 1 == lengths[i]:
+                        counts[i] += counts[j]
+
+        max_length = max(lengths)
+        return sum(counts[i] for i in range(len(nums)) if lengths[i] == max_length)
     
 
 def main():         # função principal
@@ -26,14 +27,14 @@ def main():         # função principal
     print("Case 1")
     print("Input: nums = [1,3,5,4,7]")
     n = [1,3,5,4,7]
-    print("Output: ", s.findLengthOfLCIS(n))
+    print("Output: ", s.findNumberOfLIS(n))
     print("Expected: 2")
 
     # case 2
     print("Case 2")
     print("Input: nums = [2,2,2,2,2]")
     n = [2,2,2,2,2]
-    print("Output: ", s.findLengthOfLCIS(n))
+    print("Output: ", s.findNumberOfLIS(n))
     print("Expected: 5")
 
 
