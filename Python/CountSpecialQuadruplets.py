4c8d198a274f6a7c8ef73bb440a473390594a2e7
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-04-11 18:07:39 -0300

    First try, failed case 4

--- Python/CountSpecialQuadruplets.py
+++ Python/CountSpecialQuadruplets.py
@@ -1,7 +1,24 @@
 class Solution:
     def countQuadruplets(self, nums) -> int:
 
-        return 2
+        # soma dos elmentos
+        soma = int(0)
+
+        # contador das combinações
+        cont = int(0)
+
+        # Percorre os elementos da lista, começando do segundo elemento e indo até o ante-penultimo
+        for i in range(1, len(nums)-2):
+            # Se o elemento atual for maior que o elemento anterior, a sequência continua
+            if nums[i] > nums[i - 1]:
+                # Se o elemento atual for menor que o proximo elemento, a sequência continua
+                if nums[i] < nums[i + 1]:
+                    soma = nums[i - 1] + nums[i] + nums[i + 1]
+                    
+                    if soma == nums[i + 2]:
+                         cont = cont + 1
+
+        return cont
 
 def main():         # função principal
 
