b7447d3ddeabba517ecfd61e8c5662701401db00
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-04-12 13:53:44 -0300

    Base code

---
+++ Python/CountSquareSumTriples.py
@@ -0,0 +1,40 @@
+class Solution:
+    def unequalTriplets(self, nums) -> int:
+
+        count = 0           # contador para quando as condições são satisfeitas
+        n = len(nums)        # quantidade de elementos da array
+
+        # nums[i] != nums[j], nums[i] != nums[k], and nums[j] != nums[k]
+
+        # itere sobre todas as possíveis combinações
+        for i in range(n - 2):
+            for j in range(i + 1, n - 1):
+                for k in range(j + 1, n):
+                    if nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k]:
+                        count += 1
+                            
+        return count
+    
+
+def main():         # função principal
+
+    s = Solution()      # instanciando o objeto
+
+    # case 1
+    print("Case 1")
+    print("Input: n = 5")
+    n = 5
+    print("Output: ", s.unequalTriplets(n))
+    print("Expected: 2")
+
+    # case 2
+    print("Case 2")
+    print("Input: n = 10")
+    n = 10
+    print("Output: ", s.unequalTriplets(n))
+    print("Expected: 4")
+
+
+if __name__ == "__main__":
+    main()
+    
